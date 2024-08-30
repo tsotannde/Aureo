16756cb9b93f9233218930fb3f86073b97b9b290
Adebayo Sotannde <me@adebayosotannde.com>
2024-08-30 17:00:01 -0400

    Initial Commit

---
+++ Aureo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aureo
+//
+//  Created by Adebayo Sotannde on 8/30/24.
+//
+
+import UIKit
+import LinkKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
